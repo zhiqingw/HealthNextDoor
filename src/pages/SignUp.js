05439e1c2ca9f56e7904473a6135aec8de951450
ElliotXue <61861224+ElliotXue@users.noreply.github.com>
2020-05-15 21:44:38 +1000

    Revert "sign up"
    This reverts commit d30ad890907576c70889b0dd2e2b85372cd3df5a.

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Button from "../components/Button";
-import {loginCheck, signupCheck} from "../api";
 
 export default function Login() {
   return (
@@ -36,5 +35,17 @@ function onSubmit() {
   var username = document.getElementById("username");
   var password = document.getElementById("password");
   var confirmed_password = document.getElementById("confirmed_password");
-  signupCheck(username.value, password.value, confirmed_password.value);
+
+  if (username.value === "") {
+    alert("please input a username!");
+  } else if (password.value === "") {
+    alert("please input a password!");
+  } else if (confirmed_password.value === "") {
+    alert("please confirm your password!");
+    //window.location.href="welcome.html";
+  } else if (password.value !== confirmed_password.value) {
+    alert("password and confirmed password does not match");
+  } else if (password.value === confirmed_password.value) {
+    alert("Successful!");
+  }
 }
