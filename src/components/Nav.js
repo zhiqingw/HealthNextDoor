dffcc57043866ac351e8f86528c883ac64cf3f70
zhiqingw <zhiqingw@student.unimelb.edu.au>
2020-05-16 21:16:45 +0800

    url

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -43,7 +43,7 @@ function showUser(){
 
     if(username){
         return(
-            window.location.assign(`https://localhost:3000/user-management/${window.sessionStorage.getItem("username")}`)
+            window.location.assign(`http://localhost:3000/user-management/${window.sessionStorage.getItem("username")}`)
         );
     }
 }
--- src/pages/User.js
+++ src/pages/User.js
@@ -53,7 +53,7 @@ export default function Users() {
 }
 export function Logout(){
     window.sessionStorage.removeItem("username");
-    window.location.assign('https://localhost:3000')
+    window.location.assign('http://localhost:3000/')
 }
 
 function User(user) {
