9b2190178d7e276ec6314b9c3d5404fbd312e81f
gmy0516 <56905209+gmy0516@users.noreply.github.com>
2020-05-28 19:35:39 +1000

    tolist

--- src/pages/ToList.js
+++ src/pages/ToList.js
@@ -1,19 +1,24 @@
 import React from "react";
 import {UserExtendedList} from "./User";
-import {acceptReq, acceptReqPatient, updateUser, useCaregivers, useUser} from "../api";
+import {acceptReq, acceptReqPatient, updateUser, useCaregivers, usePatients, useUser} from "../api";
 import Loading from "../components/Loading";
 import {Caregiver} from "./Caregivers";
 
+
 export default function ToList() {
-    var data = sessionStorage.getItem("listing")
-    var dict_data = JSON.parse(data)
+    var data = sessionStorage.getItem("listing");
+    var dict_data = JSON.parse(data);
     console.log(data);
+    const { loading_c, caregivers, error_c } = useCaregivers();
+    const { loading_p, patients, error_p} = usePatients();
     const { loading, user, error} = useUser();
-    console.log(user);
-    if (loading) {
+    var dict_caregivers;
+    console.log(dict_data);
+    console.log(caregivers);
+    if (loading || loading_p || loading_c) {
         return Loading();
     }
-    if (error) {
+    if (error || error_p || error_c) {
         return <p>Something went wrong: {error.message}</p>;
     }
     let username;
@@ -22,7 +27,7 @@ export default function ToList() {
     index = username.lastIndexOf('/');
     let name;
     name = username.slice(index+1);
-    if(dict_data.identity === "Caregiver") {
+    if(dict_data.identity === "caregiver") {
         return (
             <div>
                 <h1>List</h1>
@@ -31,7 +36,7 @@ export default function ToList() {
                     {user.filter(person => person.username === name).map(filteredPerson => (
                         filteredPerson.orderList.map(name => (
                                 <p>
-                                    <button onClick={() => toUserPage(name)}>
+                                    <button onClick={() => toPatientPage(name, patients)}>
                                         {name}
                                     </button>
                                 </p>
@@ -43,7 +48,7 @@ export default function ToList() {
                     {user.filter(person => person.username === name).map(filteredPerson => (
                         filteredPerson.receiveReq.map(name => (
                             <p>
-                                <button onClick={() => toUserPage(name)}>
+                                <button onClick={() => toPatientPage(name)}>
                                     {name}
                                 </button>
                                 <button onClick={() => accept(name)}>
@@ -58,7 +63,7 @@ export default function ToList() {
                     {user.filter(person => person.username === name).map(filteredPerson => (
                         filteredPerson.orderHistory.map(name => (
                             <p>
-                                <button onClick={() => toUserPage(name)}>
+                                <button onClick={() => toPatientPage(name)}>
                                     {name}
                                 </button>
                             </p>
@@ -76,7 +81,7 @@ export default function ToList() {
                     {user.filter(person => person.username === name).map(filteredPerson => (
                         filteredPerson.orderList.map(name => (
                             <p>
-                                <button onClick={() => toUserPage(name)}>
+                                <button onClick={() => toCaregiverPage(name,caregivers)}>
                                     {name}
                                 </button>
                             </p>
@@ -88,7 +93,7 @@ export default function ToList() {
                     {user.filter(person => person.username === name).map(filteredPerson => (
                         filteredPerson.sentReq.map(name => (
                             <p>
-                                <button onClick={() => toUserPage(name)}>
+                                <button onClick={() => toCaregiverPage(name)}>
                                     {name}
                                 </button>
                             </p>
@@ -100,7 +105,7 @@ export default function ToList() {
                     {user.filter(person => person.username === name).map(filteredPerson => (
                         filteredPerson.orderHistory.map(name => (
                             <p>
-                                <button onClick={() => toUserPage(name)}>
+                                <button onClick={() => toCaregiverPage(name)}>
                                     {name}
                                 </button>
                             </p>
@@ -132,9 +137,25 @@ function accept(name){
 
 }
 
-function toUserPage(name){
-    window.location.assign  (`http://localhost:3000/CaregiverInformation/${name}`);
+function toCaregiverPage(name, caregivers){
+
+    //var string_type_username = JSON.stringify(caregiver.username);
+
+    {caregivers.map(caregiver =>{
+        if(caregiver.username===name){
+            var string_type = JSON.stringify(caregiver);
+            sessionStorage.setItem("personalInformation",string_type);
+            //sessionStorage.setItem("caregiverUsername", string_type_username);
+            window.location.assign(`http://localhost:3000/CaregiverInformation/${caregiver.username}`);
+        }
+    })}
+    //window.location.assign  (`http://localhost:3000/CaregiverInformation/${name}`);
+    //console.log(caregivers);
+    //console.log(name);
 }
 
+function toPatientPage(name, patients){
+
+}
 
 
