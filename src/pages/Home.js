75555eddf8b50f2534837dabca0c9505dc20fc66
zhiqingw <zhiqingw@student.unimelb.edu.au>
2020-06-03 22:37:34 +0800

    rating

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,27 +3,8 @@ import HomePage from "../components/HomePage";
 
 export default function Home() {
     return (
-        <body  className="homepage_form">
-        <div className={"h1_form"}>
-            <h1> HEALTH NEXT DOOR</h1>
+        <div>
+            <HomePage/>
         </div>
-        <div className={"h3_form"}>
-            <h3> Care about your and your families! </h3>
-        </div>
-
-        <div className={"blank"}>
-            <h6>
-                Health Next Door would like to provide you a convenient way
-                to approach the modern healthcare facility!
-            </h6>
-            <p>
-                Hope you can enjoy our services! Cheers!
-            </p>
-        </div>
-
-        <div className={"credit_form"}>
-            <p>2020 HealthNextDoor © All Rights Reserved</p>
-        </div>
-        </body>
     );
 }
