e3b53a31f919c224ec29df11ceb154cbad426b30
zhiqingw <zhiqingw@student.unimelb.edu.au>
2020-06-07 01:44:23 +0800

    Update searchCaregivers.js

--- src/pages/searchCaregivers.js
+++ src/pages/searchCaregivers.js
@@ -2,6 +2,7 @@ import React from "react";
 import {getCaregivers} from "../api";
 import Button from "../components/Button";
 import {getCaregiverInformation, SubmitFilter} from "./Caregivers";
+import ReactStars from "react-rating-stars-component";
 
 export default function SearchCaregivers() {
     var data = sessionStorage.getItem("targetData")
@@ -10,22 +11,36 @@ export default function SearchCaregivers() {
         <div>
             <div className="list_style">
                 <h1>Caregivers List</h1>
-
+                <div id="careGiverList">
                 {dict_data.map(caregiver => (
                     <div className="list">
                         <button id="listbutton" onClick={()=>getCaregiverInformation(caregiver)}>
-                            Firstname : {caregiver.first_name}<br/><br/>
-                            Lastname : {caregiver.last_name}<br/><br/>
-                            Gender : {caregiver.gender}<br/><br/>
-                            Age : {caregiver.age}<br/><br/>
-                            Address : {caregiver.address}<br/><br/>
-                            Salary : {caregiver.salary}<br/><br/>
-                            Working Experience : {caregiver.working_experience}<br/><br/>
-                            Contact Me : {caregiver.contact_information}<br/><br/>
-                            username: {caregiver.username}<br/><br/>
+                            <div className="block_list">
+                                <div>
+                                    <div id="name">
+                                        {caregiver.first_name} {caregiver.last_name}<br/><br/>
+                                    </div>
+                                    <div id="intro">
+                                        introduction : {caregiver.introduction}
+                                    </div>
+                                    <div id="as">
+                                        Address : {caregiver.address} &nbsp;&nbsp;
+                                        Salary : {caregiver.salary}<br/><br/>
+                                    </div>
+                                    <ReactStars
+                                        count={caregiver.rate}
+                                        size={24}
+                                        edit={false}
+                                        half={true}
+
+                                        color1={'#ffd700'} />
+                                </div>
+                                <img className={"photo"}  src={caregiver.image} ></img>
+                            </div>
                         </button>
                     </div>
                 ))}
+                </div>
 
 
             </div>
