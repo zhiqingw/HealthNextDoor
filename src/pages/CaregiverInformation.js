7ecfee3c5d27a3d100a9ae67651f4c56c85d4e8e
gmy0516 <56905209+gmy0516@users.noreply.github.com>
2020-06-06 18:12:32 +1000

    caregiverInformation

--- src/pages/CaregiverInformation.js
+++ src/pages/CaregiverInformation.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import {useCaregivers, updateCaregiver, deleteCaregiver, sendRequest, sentRequestPatient, useUser} from "../api";
-import Box from 'react-styled-box';
+
 import Button from "../components/Button";
 import Filter from "../components/Filter";
 import HomePage from "../components/HomePage";
 import Loading from "../components/Loading";
 import ReactStars from "react-rating-stars-component";
 import Upload from "../components/Upload";
+import anonymousIcon from "../pictures/anoy.png";
 export default function CaregiverInformation(){
     let username;
     username = window.location.pathname;
@@ -58,7 +59,7 @@ export default function CaregiverInformation(){
                 <div>
                     <form className={"image_block"}>
                     </form>
-                    <form className={"caregiver_infor_style"}>
+                    <div className={"caregiver_infor_style"}>
 
                         {caregivers.filter(person => person.username === caregiver_name).map(dict_data => (
                         <form>
@@ -81,7 +82,7 @@ export default function CaregiverInformation(){
                                 color1={'#ffd700'} />
                         </form>
                             ))}
-                    </form>
+                    </div>
                     <div>
                     review:
                         {caregivers.filter(person => person.username === caregiver_name).map(dict_data => (
@@ -116,7 +117,9 @@ export default function CaregiverInformation(){
                     <div className={"caregiver_infor_style"}>
                         {caregivers.filter(person => person.username === caregiver_name).map(dict_data => (
                             <form>
+                                <form>
                                 <img className={"icon"}  src={dict_data.image} ></img><br/><br/>
+
                                 <i className="fa fa-user" aria-hidden="true">{dict_data.first_name} {dict_data.last_name}</i><br/><br/>
 
                                 Gender : {dict_data.gender}<br/><br/>
@@ -125,24 +128,31 @@ export default function CaregiverInformation(){
                                 Salary : {dict_data.salary}<br/><br/>
                                 Working Experience : {dict_data.working_experience}<br/><br/>
                                 Contact Me : {dict_data.contact_information}<br/><br/>
-                                <ReactStars
-                                    count={dict_data.rate}
-                                    size={24}
-                                    edit={false}
-                                    half={true}
+                                Rating :
+                                </form>
+                                <form className={"stars_form"}>
+                                    <ReactStars
+                                        count={dict_data.rate}
+                                        size={24}
+                                        edit={false}
+                                        half={true}
+                                        color1={'#ffd700'} /></form>
 
-                                    color1={'#ffd700'} />
                             </form>
                         ))}
                     </div>
-                    <div>
-                        review:
+                    <div className={"comment_style"}>
+                        <h4>Comment:</h4>
                         {caregivers.filter(person => person.username === caregiver_name).map(dict_data => (
                             dict_data.comment.map( comment => (
-                                <p>
-                                    Anonymous: {comment}
+                                <p className={"comment_p_style"}>
+                                    <img src={anonymousIcon} width={"25px"}></img>  &nbsp;
+                                    Anonymous: <br/><br/>{comment}
+                                    <p className={"comment_p_style"}>
                                     <hr />
+                                    </p>
                                 </p>
+
                             ))))}
                     </div>
                 </div>
